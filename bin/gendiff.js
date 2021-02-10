9fbae123cd26422e8023f00e487c34a7e766466e
Maria Pushkareva <dautova.maria92@gmail.com>
2021-02-10 13:50:46 +0100

    delete node_modules from git

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,6 +3,6 @@ import program from 'commander';
 
 program
   .version('0.0.1')
-  .description('Compares two configuration files and shows a difference.');
-
-program.parse(process.argv);
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format [type]', 'output format')
+  .parse(process.argv);
