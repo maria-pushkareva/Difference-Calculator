8ef1259e03492ffe20b6f59b2fee8b7a26a9dfad
Maria Pushkareva <dautova.maria92@gmail.com>
2021-03-13 20:38:31 +0100

    add first test

---
+++ __tests__/index.test.js
@@ -0,0 +1,21 @@
+import gendiff from '../src/index.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+test('gendiff', () => {
+  const __filename = fileURLToPath(import.meta.url);
+  const __dirname = dirname(__filename);
+
+  const filepath1 = path.join(__dirname, '..', '__fixtures__', 'file1_flat.json');
+  const filepath2 = path.join(__dirname, '..', '__fixtures__', 'file2_flat.json');
+  const result = [
+    '  - follow: false',
+    '    host: hexlet.io',
+    '  - proxy: 123.234.53.22',
+    '  - timeout: 50',
+    '  + timeout: 20',
+    '  + verbose: true',
+  ];
+  expect(gendiff(filepath1, filepath2)).toEqual(`{\n${result.join('\n')}\n}`);
+});
