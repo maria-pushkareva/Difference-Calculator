84be34f8eaf3b0643851a5f2ead11eda70004f1f
Maria Pushkareva <dautova.maria92@gmail.com>
2021-06-06 00:04:57 +0200

    add mapping

--- src/index.js
+++ src/index.js
@@ -1,19 +1,24 @@
 import fs from 'fs';
 import path from 'path';
 import compareJson from './compareJson.js';
+import compareYaml from './compareYaml.js';
 
 const makeAbsolutePath = (filepath) => path.resolve(process.cwd(), filepath);
 const getFileData = (filepath) => fs.readFileSync(makeAbsolutePath(filepath));
 
+const mapping = {
+  json: compareJson,
+  yaml: compareYaml,
+  yml: compareYaml,
+}
+
 const gendiff = (filepath1, filepath2) => {
   const fileData1 = getFileData(filepath1);
   const fileData2 = getFileData(filepath2);
-  const result = compareJson(fileData1, fileData2);
+  const ext = path.extname(filepath2);
+  const compareFunction = mapping[ext].slice(1);
+  const result = compareFunction(fileData1, fileData2);
   return `{\n${result.join('\n')}\n}`;
 };
-
-/* exists(x, (e) => {
-    console.log(e ? 'it exists' : 'no passwd!');
-  }); */
-
+console.log()
 export default gendiff;
