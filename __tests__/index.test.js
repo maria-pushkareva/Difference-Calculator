1ea1f1bd329956ecbe47873c3fea41803e600993
Maria Pushkareva <dautova.maria92@gmail.com>
2021-06-08 13:07:34 +0200

    udate tests for nested structures

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,5 +1,6 @@
 import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
+import fs from 'fs';
 
 import gendiff from '../src/index.js';
 
@@ -7,23 +8,16 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 const generatePathName = (fileName) => path.join(__dirname, '..', '__fixtures__', fileName);
 
-const result = [
-  '  - follow: false',
-  '    host: hexlet.io',
-  '  - proxy: 123.234.53.22',
-  '  - timeout: 50',
-  '  + timeout: 20',
-  '  + verbose: true',
-];
+const result = fs.readFileSync(generatePathName('result.txt'), 'utf-8');
 
 const testComparing = [
   ['file1_flat.json', 'file2_flat.json'],
-  ['file1_flat.yaml', 'file2_flat.yaml'],
+  ['file1.yaml', 'file2.yaml'],
 ];
 
 test.each(testComparing)('gendiff', (file1, file2) => {
   const filepath1 = generatePathName(file1);
   const filepath2 = generatePathName(file2);
 
-  expect(gendiff(filepath1, filepath2)).toEqual(`{\n${result.join('\n')}\n}`);
+  expect(gendiff(filepath1, filepath2)).toBe(result);
 });
