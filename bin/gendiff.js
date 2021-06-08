6d993a40f1a23e6d9ccc9e8326cfcf74839cfdb0
Maria Pushkareva <dautova.maria92@gmail.com>
2021-06-08 16:23:04 +0200

    changes for eslint

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,6 +6,8 @@ program
   .version('0.0.1')
   .arguments('<filepath1> <filepath2>')
   .description('Compares two configuration files and shows a difference.')
-  .option('-f, --format [type]', 'output format')
-  .action((filepath1, filepath2, options) => console.log(gendiff(filepath1, filepath2, options.format)))
+  .option('-f, --format [type]', 'output format', 'stylish')
+  .action((filepath1, filepath2, options) => {
+    console.log(gendiff(filepath1, filepath2, options.format));
+  })
   .parse(process.argv);
